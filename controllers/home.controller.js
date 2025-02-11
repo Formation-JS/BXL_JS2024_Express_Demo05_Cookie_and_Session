b9950bc85cf417ed35180be18608757b24e52907
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-11 16:09:15 +0100

    Give Cookie 🍪

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,11 +1,27 @@
 const homeController = {
 
     index: (req, res) => {
-        
+
+        // Création / Modification d'un cookie chez le client ! (Attention sur "res")
+        // - Sans config
+        res.cookie("Number", 42);
+        // - Avec config
+        res.cookie("Demo", "Ceci est un exemple", {
+            signed: true,
+            httpOnly: true,
+            sameSite: 'strict'
+        });
+
         res.render('home/index');
     },
 
     about: (req, res) => {
+
+        // Lecture d'un cookie depuis chez le client ! (Attention sur "req")
+        // - Cookie
+        console.log(req.cookies);
+        // - Cookie signé
+        console.log(req.signedCookies);
 
         res.render('home/about');
     }
