22e444c8ecafd2866ea34e683e055a38c8c7e226
Aurélien St <aurelien.strimelle@bstorm.be>
2025-02-14 09:19:20 +0100

    View for login 🐞

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,14 @@
 const authController = {
 
     loginGet: (req, res) => {
-        res.sendStatus(501);
+        res.render('auth/login');
     },
     loginPost: (req, res) => {
         res.sendStatus(501);
     },
 
     registerGet: (req, res) => {
-        res.sendStatus(501);
+        res.render('auth/register');
     },
     registerPost: (req, res) => {
         res.sendStatus(501);
